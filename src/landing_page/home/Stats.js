487f3cb19f3c441189e4e5bdc44c8517b343f8c0
rahul123-sardar <rahulsardarr21@gmail.com>
2026-01-06 14:18:29 +0530

    features

---
+++ src/landing_page/home/Stats.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Stats(){
+    return
+    (
+        <h1>Awards</h1>
+    )
+}
+
+export default Stats();
---
+++ src/landing_page/pricing/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Hero(){
+    return
+    (
+        <h1>Awards</h1>
+    )
+}
+
+export default Hero
