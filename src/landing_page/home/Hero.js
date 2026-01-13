2048cff1bc91268aaea36f78e8354e9e120f7b14
rahul123-sardar <rahulsardarr21@gmail.com>
2026-01-13 22:10:15 +0530

    Add features

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Hero(){
     return (
     <div className="container p-5 mb-5">
@@ -7,7 +7,7 @@ function Hero(){
             <img src="media/images/homeHero.png" alt="Hero Image"/>
             <h1 className="mt-5">Invest in everything</h1>
             <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-            <button className="p-3 btn btn-primary fs-5 mb-5" style={{width:"30%", margin:"0 auto"}}>Signup Now</button>
+           <Link to="/signup"><button className="p-3 btn btn-primary fs-5 mb-5" style={{width:"30%", margin:"0 auto"}}>Signup Now</button></Link> 
         </div>
     </div>
     );
